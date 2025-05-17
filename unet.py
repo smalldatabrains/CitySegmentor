189ee52df3b09a01cf2b9e713c8c7d89204dc81a
smalldatabrains <smalldatabrains@gmail.com>
2025-05-17 11:21:54 +0200

    unet forward implementation

--- unet.py
+++ unet.py
@@ -38,3 +38,19 @@ class UNET(nn.Module):
 
         self.bottleneck = Conv2Layer(features[-1], features[-1]*2)
         self.final_conv = nn.Conv2d(features[0], out_channels,kernel_size=1)
+
+    def forward(self,x):
+        skip_connections=[]
+        for down in self.downs:
+            x=down(x)
+            skip_connections.append(x)
+            x= self.pool(x)
+        x=self.bottleneck(x)
+        skip_connections=skip_connections[::-1] #reverse the list
+        
+        for idx in range (0, len(self.ups),2):
+            x=self.ups[idx](x)
+            skip_connection=skip_connections[idx//2]
+            concat_skip=torch.cat((skip_connection,x),dim=1)
+            x=self.ups[idx+1](concat_skip)
+        return self.final_conv(x)
